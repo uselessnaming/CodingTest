9d8fef8f0b4b003aec4a6d53ff3e8681c028920d
uselessNaming <seohojon@naver.com>
2025-03-25 10:30:15 +0900

    알고리즘에 대한 설명 추가

--- 프로그래머스/ExpressN.kt
+++ 프로그래머스/ExpressN.kt
@@ -3,6 +3,11 @@ package 프로그래머스
 import java.lang.Math.pow
 import kotlin.math.min
 
+// 재귀적으로 탐색한다고 생각해서 단순 dfs라고 생각했지만
+// 원리를 생각해보면 각 단계별로 진행한 연산의 값과 cnt를 가지고
+// 다음 단계로 넘어가서 값을 비교하는 것이므로 dp의 방법으로 볼 수 있음
+// 명확하게 dfs와 dp를 구분하기는 애매한 문제라고 생각함
+
 class ExpressN {
     // answer은 최대값인 9로 시작
     var answer = 9
