61afa543afb7651dec7f3f5b95e9db71884440a8
uselessNaming <seohojon@naver.com>
2025-03-25 10:59:58 +0900

    알고리즘 설명 추가

--- 프로그래머스/Lighthouse.kt
+++ 프로그래머스/Lighthouse.kt
@@ -1,5 +1,9 @@
 package 프로그래머스
 
+// 이 문제의 경우 다른 것보다 어떻게 해야 최소한의 등대를 켤 수 있는지를 생각해야 함
+// leaf 노드는 켜질 경우 굉장히 비효율적임
+// 따라서 leaf 노드에서 출발하여 연결된 등대를 켜고 다른 등대들을 제거하는 방법으로 하는 것이 좋음
+
 class Lighthouse {
     fun solution(n: Int, lighthouse: Array<IntArray>): Int {
         val graph = mutableMapOf<Int, MutableSet<Int>>()
