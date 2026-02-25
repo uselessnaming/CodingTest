3b93acb7e1eb02fcee033dced80b80a7d2f74d28
uselessNaming <seohojon@naver.com>
2026-02-25 12:00:16 +0900

    15651 N과 M (3)

--- Main.kt
+++ Main.kt
@@ -8,5 +8,5 @@ fun main() {
 //    solution.run()
 
     // 백준
-    NAndMSeries().apply{secondRun()}
+    NAndMSeries().apply{thirdRun()}
 }
--- 백준/15649_series_n_and_m.kt
+++ 백준/15649_series_n_and_m.kt
@@ -1,6 +1,11 @@
 package 백준
 
+import java.time.LocalDateTime
+
 class NAndMSeries {
+    private var secondAnswers = mutableSetOf<List<Int>>()
+    private var thirdAnswers = mutableSetOf<List<Int>>()
+
     /** 15649 n과 m (1) */
     fun firstRun() {
         val (n, m) = readln().split(" ").map { it.toInt() }
@@ -23,21 +28,19 @@ class NAndMSeries {
         }
     }
 
-    private var answers = mutableSetOf<List<Int>>()
-
     /** 15649 n과 m (2) */
     fun secondRun() {
         val (n, m) = readln().split(" ").map { it.toInt() }
         val visited = BooleanArray(n + 1) { false }
         dfsAddValue(0, m, emptyList(), visited)
-        answers.forEach { answer ->
+        secondAnswers.forEach { answer ->
             println(answer.joinToString(" "))
         }
     }
 
     private fun dfsAddValue(depth: Int, limit: Int, answer: List<Int>, visited: BooleanArray) {
         if (depth == limit) {
-            answers.add(answer.sorted())
+            secondAnswers.add(answer.sorted())
             return
         }
 
@@ -47,6 +50,25 @@ class NAndMSeries {
                 dfsAddValue(depth + 1, limit, answer + i, visited)
                 visited[i] = false
             }
+        }
+    }
+
+    /** 15651 n과 m (3) */
+    fun thirdRun() {
+        val now = System.currentTimeMillis()
+        val (n, m) = readln().split(" ").map { it.toInt() }
+        thirdDfs(0, n, m, emptyList())
+        println(thirdAnswers.joinToString("\n") { it.joinToString(" ") })
+    }
+
+    private fun thirdDfs(depth: Int, end: Int, limit: Int, answer: List<Int>) {
+        if (depth == limit) {
+            thirdAnswers.add(answer)
+            return
+        }
+
+        for (i in 1..end) {
+            thirdDfs(depth + 1, end, limit, answer + i)
         }
     }
 }
