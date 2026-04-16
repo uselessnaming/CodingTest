f03be6012b374ca153887db9b113c34f32eafe95
uselessNaming <seohojon@naver.com>
2026-04-16 11:02:47 +0900

    프로그래머스 표 병합
    union find 를 기반으로 구현할 수 있음
    병합하는 것에 대해서 union find에 대한 생각을 떠올리면 됨

--- Main.kt
+++ Main.kt
@@ -3,11 +3,11 @@ import 프로그래머스.*
 import java.util.*
 
 fun main() {
-//    //프로그래머스
-//    val n = 5
-//    val answer = NumberOfBrackets().solution(n)
-//    print(answer)
+    //프로그래머스
+    val commands = arrayOf("UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4")
+    val answer = MergeTable().solution(commands)
+    print(answer.toList())
 
-    // 백준
-    StartAndLink().apply { run() }
+//    // 백준
+//    StartAndLink().apply { run() }
 }
