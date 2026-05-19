7cecf7feefbcbbb05737e0651ffb9d5b86e3e688
uselessNaming <seohojon@naver.com>
2026-05-19 10:57:32 +0900

    프로그래머스 카드 짝 맞추기
    완전 탐색 문제
    주석은 처음 해결하고자 하는 방향
    본 코드는 AI의 리뷰를 받고 진행한 코드
    방향성은 맞았으나 구현하는 과정에서 생각한 부분이 틀렸음
    다시 시도

--- 프로그래머스/카드 짝 맞추기.kt
+++ 프로그래머스/카드 짝 맞추기.kt
@@ -3,172 +3,436 @@ package 프로그래머스
 import java.util.Deque
 import java.util.LinkedList
 import kotlin.math.min
+import java.util.*
+import kotlin.math.min
+
+
+//class MatchingCard {
+//    private val SIZE = 4
+//    private val dx = intArrayOf(0, 0, -1, 1)
+//    private val dy = intArrayOf(-1, 1, 0, 0)
+//    private lateinit var board: Array<IntArray>
+//    private lateinit var cards: List<Pair<Int, Int>>
+//
+//    private var answer = Int.MAX_VALUE
+//
+//    fun solution(board: Array<IntArray>, r: Int, c: Int): Int {
+//        this.board = board
+//        cards = getAllCards()
+//        println(cards)
+//
+//        val visited = BooleanArray(cards.size) { false }
+//
+//        dfs(r, c, visited, 0)
+//
+//        return answer
+//    }
+//
+//    private fun dfs(startX: Int, startY: Int, visited: BooleanArray, score: Int) {
+//        println("dfs : $startX $startY $score")
+//        println("visited : ${visited.toList()}")
+//
+//        if (visited.all { it }) {
+//            answer = min(answer, score)
+//            println("fetch answer : $answer")
+//            return
+//        }
+//
+//        for (idx in cards.indices) {
+//            if (visited[idx]) continue
+//
+//            val (targetX, targetY) = cards[idx]
+//            println("card : ${cards[idx]}")
+//            println("$startX $startY $targetX $targetY")
+//
+//            val next =
+//                getMinScore(startX, startY, targetX, targetY, cards.filterIndexed { idx, value -> visited[idx] })
+//            println("next : $next")
+//
+//            visited[idx] = true
+//
+//            for ((nextX, nextY, nextScore) in next) {
+//                val (matchX, matchY, matchScore) = getMinScore(
+//                    nextX,
+//                    nextY,
+//                    board[nextX][nextY],
+//                    cards.filterIndexed { idx, value -> visited[idx] })[0]
+//                println("match : $matchX $matchY $matchScore")
+//
+//                val matchIdx = cards.indexOf(matchX to matchY)
+//                println("match idx : $matchIdx")
+//                visited[matchIdx] = true
+//                dfs(matchX, matchY, visited, score + nextScore + matchScore)
+//                visited[matchIdx] = false
+//            }
+//
+//            visited[idx] = false
+//            println("for end visited : ${visited.toList()}")
+//        }
+//    }
+//
+//    private fun getAllCards(): List<Pair<Int, Int>> {
+//        val result = mutableListOf<Pair<Int, Int>>()
+//        for (x in 0 until SIZE) {
+//            for (y in 0 until SIZE) {
+//                if (board[x][y] != 0) result.add(x to y)
+//            }
+//        }
+//        return result
+//    }
+//
+//    private fun getMinScore(
+//        startX: Int,
+//        startY: Int,
+//        targetX: Int,
+//        targetY: Int,
+//        cardVisited: List<Pair<Int, Int>>
+//    ): List<Triple<Int, Int, Int>> {
+//        if (startX to startY == targetX to targetY) return listOf(Triple(startX, startY, 1))
+//
+//        val visited = Array(SIZE) { BooleanArray(SIZE) { false } }
+//        visited[startX][startY] = true
+//        cardVisited.forEach { (x, y) ->
+//            visited[x][y] = true
+//        }
+//
+//        val queue: Deque<List<Int>> = LinkedList()
+//        queue.add(listOf(startX, startY, 1))
+//
+//        while (queue.isNotEmpty()) {
+//            val (x, y, score) = queue.removeFirst()
+//
+//            for (idx in 0 until 4) {
+//                val nx = x + dx[idx]
+//                val ny = y + dy[idx]
+//
+//                if (nx in 0 until SIZE && ny in 0 until SIZE) {
+//                    if (targetX to targetY == nx to ny) {
+//                        return listOf(Triple(nx, ny, score + 1))
+//                    }
+//
+//                    if (visited[nx][ny]) continue
+//
+//                    visited[nx][ny] = true
+//                    queue.add(listOf(nx, ny, score + 1))
+//                }
+//            }
+//
+//            if (x == 1) {
+//                if (visited[2][y]) continue
+//
+//                if (!visited[3][y]) {
+//                    if (targetX to targetY == 3 to y && startX to startY != 3 to y) {
+//                        return listOf(Triple(3, y, score + 1))
+//                    } else {
+//                        queue.add(listOf(3, y, score + 1))
+//                        visited[3][y] = true
+//                        continue
+//                    }
+//                }
+//            } else if (x == 2) {
+//                if (visited[1][y]) continue
+//
+//                if (!visited[0][y]) {
+//                    if (targetX to targetY == 0 to y && startX to startY != 0 to y) {
+//                        return listOf(Triple(0, y, score + 1))
+//                    } else {
+//                        queue.add(listOf(0, y, score + 1))
+//                        visited[0][y] = true
+//                        continue
+//                    }
+//                }
+//            }
+//
+//            if (y == 1) {
+//                if (visited[x][2]) continue
+//
+//                if (!visited[x][3]) {
+//                    if (targetX to targetY == x to 3 && startX to startY != x to 3) {
+//                        return listOf(Triple(x, 3, score + 1))
+//                    } else {
+//                        queue.add(listOf(x, 3, score + 1))
+//                        visited[x][3] = true
+//                        continue
+//                    }
+//                }
+//            } else if (y == 2) {
+//                if (visited[x][1]) continue
+//
+//                if (!visited[x][0]) {
+//                    if (targetX to targetY == x to 0 && startX to startY != x to 0) {
+//                        return listOf(Triple(x, 0, score + 1))
+//                    } else {
+//                        queue.add(listOf(x, 0, score + 1))
+//                        visited[x][0] = true
+//                        continue
+//                    }
+//                }
+//            }
+//        }
+//
+//        return emptyList()
+//    }
+//
+//    private fun getMinScore(
+//        startX: Int,
+//        startY: Int,
+//        targetScore: Int,
+//        cardVisited: List<Pair<Int, Int>>
+//    ): List<Triple<Int, Int, Int>> {
+//        val visited = Array(SIZE) { BooleanArray(SIZE) { false } }
+//        visited[startX][startY] = true
+//        cardVisited.forEach { (x, y) ->
+//            visited[x][y] = true
+//        }
+//
+//        val queue: Deque<List<Int>> = LinkedList()
+//        queue.add(listOf(startX, startY, 1))
+//
+//        while (queue.isNotEmpty()) {
+//            val (x, y, score) = queue.removeFirst()
+//
+//            for (idx in 0 until 4) {
+//                val nx = x + dx[idx]
+//                val ny = y + dy[idx]
+//
+//                if (nx in 0 until SIZE && ny in 0 until SIZE) {
+//                    if (board[nx][ny] == targetScore && startX to startY != nx to ny) {
+//                        return listOf(Triple(nx, ny, score + 1))
+//                    }
+//
+//                    if (visited[nx][ny]) continue
+//
+//                    visited[nx][ny] = true
+//                    queue.add(listOf(nx, ny, score + 1))
+//                }
+//            }
+//
+//            if (x == 1) {
+//                if (visited[2][y]) continue
+//
+//                if (!visited[3][y]) {
+//                    if (board[3][y] == targetScore) {
+//                        return listOf(Triple(3, y, score + 1))
+//                    } else {
+//                        queue.add(listOf(3, y, score + 1))
+//                        visited[3][y] = true
+//                        continue
+//                    }
+//                }
+//            } else if (x == 2) {
+//                if (visited[1][y]) continue
+//
+//                if (!visited[0][y]) {
+//                    if (board[0][y] == targetScore) {
+//                        return listOf(Triple(0, y, score + 1))
+//                    } else {
+//                        queue.add(listOf(0, y, score + 1))
+//                        visited[0][y] = true
+//                        continue
+//                    }
+//                }
+//            }
+//
+//            if (y == 1) {
+//                if (visited[x][2]) continue
+//
+//                if (!visited[x][3]) {
+//                    if (board[x][3] == targetScore && startX to startY != x to 3) {
+//                        return listOf(Triple(x, 3, score + 1))
+//                    } else {
+//                        queue.add(listOf(x, 3, score + 1))
+//                        visited[x][3] = true
+//                        continue
+//                    }
+//                }
+//            } else if (y == 2) {
+//                if (visited[x][1]) continue
+//
+//                if (!visited[x][0]) {
+//                    if (board[x][0] == targetScore && startX to startY != x to 0) {
+//                        return listOf(Triple(x, 0, score + 1))
+//                    } else {
+//                        queue.add(listOf(x, 0, score + 1))
+//                        visited[x][0] = true
+//                        continue
+//                    }
+//                }
+//            }
+//        }
+//
+//        return emptyList()
+//    }
+//}
 
 class MatchingCard {
-    private val SIZE = 4
-    private val dx = intArrayOf(0, 0, -1, 1)
-    private val dy = intArrayOf(-1, 1, 0, 0)
-    private lateinit var board: Array<IntArray>
-    private lateinit var cards: List<Pair<Int, Int>>
+
+    private val dx = intArrayOf(-1, 1, 0, 0)
+    private val dy = intArrayOf(0, 0, -1, 1)
 
     private var answer = Int.MAX_VALUE
 
     fun solution(board: Array<IntArray>, r: Int, c: Int): Int {
-        this.board = board
-        cards = getAllCards()
-        println(cards)
 
-        val visited = BooleanArray(cards.size) { false }
+        val cardMap = mutableMapOf<Int, MutableList<Pair<Int, Int>>>()
 
-        dfs(r, c, visited, 0)
+        for (i in 0..3) {
+            for (j in 0..3) {
+                if (board[i][j] != 0) {
+                    cardMap.getOrPut(board[i][j]) { mutableListOf() }
+                        .add(i to j)
+                }
+            }
+        }
+
+        dfs(board, r, c, cardMap.keys.toList(), 0, 0)
 
         return answer
     }
 
-    private fun dfs(startX: Int, startY: Int, visited: BooleanArray, score: Int) {
-        println("dfs : $startX $startY $score")
-        println("visited : ${visited.toList()}")
+    private fun dfs(
+        board: Array<IntArray>,
+        x: Int,
+        y: Int,
+        cards: List<Int>,
+        idx: Int,
+        cost: Int
+    ) {
 
-        if (visited.all { it }) {
-            answer = min(answer, score)
-            println("fetch answer : $answer")
+        if (cost >= answer) return
+
+        if (idx == cards.size) {
+            answer = min(answer, cost)
             return
         }
 
-        for (idx in cards.indices) {
-            if (visited[idx]) continue
+        for (card in cards) {
 
-            val (targetX, targetY) = cards[idx]
-            println("card : ${cards[idx]}")
+            if (!exists(board, card)) continue
 
-            val next =
-                getMinScore(startX, startY, board[targetX][targetY], cards.filterIndexed { idx, value -> visited[idx] })
-            println("next : $next")
+            val positions = mutableListOf<Pair<Int, Int>>()
 
-            visited[idx] = true
+            for (i in 0..3) {
+                for (j in 0..3) {
+                    if (board[i][j] == card) {
+                        positions.add(i to j)
+                    }
+                }
+            }
 
-            for ((nextX, nextY, nextScore) in next) {
-                val (matchX, matchY, matchScore) = getMinScore(
-                    nextX,
-                    nextY,
-                    board[nextX][nextY],
-                    cards.filterIndexed { idx, value -> visited[idx] })[0]
-                println("match : $matchX $matchY $matchScore")
+            val (ax, ay) = positions[0]
+            val (bx, by) = positions[1]
 
-                val matchIdx = cards.indexOf(matchX to matchY)
-                visited[matchIdx] = true
-                dfs(matchX, matchY, visited, score + nextScore + matchScore)
-                visited[matchIdx] = false
+            // A -> B
+            run {
+                val toA = bfs(board, x, y, ax, ay)
+                val toB = bfs(board, ax, ay, bx, by)
+
+                board[ax][ay] = 0
+                board[bx][by] = 0
+
+                dfs(
+                    board,
+                    bx,
+                    by,
+                    cards,
+                    idx + 1,
+                    cost + toA + toB + 2
+                )
+
+                board[ax][ay] = card
+                board[bx][by] = card
+            }
+
+            // B -> A
+            run {
+                val toB = bfs(board, x, y, bx, by)
+                val toA = bfs(board, bx, by, ax, ay)
+
+                board[ax][ay] = 0
+                board[bx][by] = 0
+
+                dfs(
+                    board,
+                    ax,
+                    ay,
+                    cards,
+                    idx + 1,
+                    cost + toA + toB + 2
+                )
+
+                board[ax][ay] = card
+                board[bx][by] = card
             }
         }
     }
 
-    private fun getAllCards(): List<Pair<Int, Int>> {
-        val result = mutableListOf<Pair<Int, Int>>()
-        for (x in 0 until SIZE) {
-            for (y in 0 until SIZE) {
-                if (board[x][y] != 0) result.add(x to y)
+    private fun exists(board: Array<IntArray>, target: Int): Boolean {
+        for (i in 0..3) {
+            for (j in 0..3) {
+                if (board[i][j] == target) return true
             }
         }
-        return result
+        return false
     }
 
-    private fun getMinScore(
-        startX: Int,
-        startY: Int,
-        targetScore: Int,
-        cardVisited: List<Pair<Int, Int>>
-    ): List<Triple<Int, Int, Int>> {
-        val visited = Array(SIZE) { BooleanArray(SIZE) { false } }
-        visited[startX][startY] = true
-        cardVisited.forEach { (x, y) ->
-            visited[x][y] = true
-        }
+    private fun bfs(
+        board: Array<IntArray>,
+        sx: Int,
+        sy: Int,
+        tx: Int,
+        ty: Int
+    ): Int {
 
-        val queue: Deque<List<Int>> = LinkedList()
-        queue.add(listOf(startX, startY, 1))
+        val visited = Array(4) { BooleanArray(4) }
+        val queue: Queue<IntArray> = LinkedList()
 
-        var cnt = 0
+        queue.add(intArrayOf(sx, sy, 0))
+        visited[sx][sy] = true
 
         while (queue.isNotEmpty()) {
-            val (x, y, score) = queue.removeFirst()
 
-            for (idx in 0 until 4) {
-                val nx = x + dx[idx]
-                val ny = y + dy[idx]
+            val (x, y, dist) = queue.poll()
 
-                if (nx in 0 until SIZE && ny in 0 until SIZE) {
-                    if (board[nx][ny] == targetScore && startX to startY != nx to ny) {
-                        return listOf(Triple(nx, ny, score + 1))
-                    }
+            if (x == tx && y == ty) {
+                return dist
+            }
 
-                    if (visited[nx][ny]) continue
+            for (d in 0 until 4) {
 
+                // 일반 이동
+                var nx = x + dx[d]
+                var ny = y + dy[d]
+
+                if (nx in 0..3 && ny in 0..3 && !visited[nx][ny]) {
                     visited[nx][ny] = true
-                    queue.add(listOf(nx, ny, score + 1))
+                    queue.add(intArrayOf(nx, ny, dist + 1))
+                }
+
+                // Ctrl 이동
+                nx = x
+                ny = y
+
+                while (true) {
+
+                    val mx = nx + dx[d]
+                    val my = ny + dy[d]
+
+                    if (mx !in 0..3 || my !in 0..3) break
+
+                    nx = mx
+                    ny = my
+
+                    if (board[nx][ny] != 0) break
+                }
+
+                if (!visited[nx][ny]) {
+                    visited[nx][ny] = true
+                    queue.add(intArrayOf(nx, ny, dist + 1))
                 }
             }
-
-            if (x > 1) {
-                for (nnx in x - 1 downTo 0) {
-                    if (board[nnx][y] != 0) {
-                        if (nnx == x - 1) break
-
-                        if (!visited[nnx][y]) {
-                            visited[nnx][y] = true
-                            queue.add(listOf(nnx, y, score + 1))
-                            break
-                        }
-                    }
-
-                    if (nnx == 0 && !visited[nnx][y]) queue.add(listOf(nnx, y, score + 1))
-                }
-            }
-
-            if (x < 2) {
-                for (nnx in x + 1 until SIZE) {
-                    if (board[nnx][y] != 0) {
-                        if (nnx == x + 1) break
-
-                        visited[nnx][y] = true
-                        queue.add(listOf(nnx, y, score + 1))
-                        break
-                    }
-
-                    if (nnx == SIZE - 1 && !visited[nnx][y]) queue.add(listOf(nnx, y, score + 1))
-                }
-            }
-
-            if (y > 1) {
-                for (nny in y - 1 downTo 0) {
-                    if (board[x][nny] != 0) {
-                        if (nny == y - 1) break
-
-                        visited[x][nny] = true
-                        queue.add(listOf(x, nny, score + 1))
-                        break
-                    }
-                    if (nny == 0 && !visited[x][nny]) queue.add(listOf(x, nny, score + 1))
-                }
-            }
-
-            if (y < 2) {
-                for (nny in y + 1 until SIZE) {
-                    if (board[x][nny] != 0) {
-                        if (nny == y + 1) break
-
-                        visited[x][nny] = true
-                        queue.add(listOf(x, nny, score + 1))
-                        break
-                    }
-
-                    if (nny == SIZE - 1 && !visited[x][nny]) queue.add(listOf(x, nny, score + 1))
-                }
-            }
-
-            cnt++
         }
 
-        return emptyList()
+        return Int.MAX_VALUE
     }
 }
