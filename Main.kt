fa99cadf4848e49aa451d32c0a12b3e82a4b29c2
uselessNaming <seohojon@naver.com>
2026-06-19 09:51:10 +0900

    프로그래머스 리프 노드 수 최대화
    문제 해결 방안을 찾지 못했음
    AI 도움받기도 실패
    아예 다시 해보기

--- Main.kt
+++ Main.kt
@@ -4,21 +4,10 @@ import kotlin.random.Random
 
 fun main() {
     //프로그래머스
-    val cost = arrayOf(
-        intArrayOf(160, 140, 120, 110, 60),
-        intArrayOf(290, 270, 260, 120, 10),
-        intArrayOf(160, 130, 120, 60, 20),
-        intArrayOf(160, 120, 80, 70, 20),
-        intArrayOf(110, 70, 60, 30, 20)
-    )
-    val hint = arrayOf(
-        intArrayOf(40, 2, 3),
-        intArrayOf(40, 5, 3),
-        intArrayOf(20, 5, 4),
-        intArrayOf(50, 5, 5)
-    )
+    val distLimit = 3
+    val splitLimit = 100
 
-    val answer = HintStage().solution(cost, hint)
+    val answer = MaxOfLeafNode().solution(distLimit, splitLimit)
     println(answer)
 
 //    // 백준
