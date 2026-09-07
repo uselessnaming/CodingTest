f0638da258a9e97d1fe00a89f1e6a2e5898c1bb3
uselessNaming <seohojon@naver.com>
2026-09-07 14:44:08 +0900

    프로그래머스 충돌위험
    예외 케이스를 더 고려하기

--- Main.kt
+++ Main.kt
@@ -4,27 +4,29 @@ import kotlin.random.Random
 
 fun main() {
     //프로그래머스
-//    val key = arrayOf(
-//        intArrayOf(0, 0, 0),
-//        intArrayOf(1, 0, 0),
-//        intArrayOf(0, 1, 1),
-//    )
-//    val lock = arrayOf(
-//        intArrayOf(1, 1, 1),
-//        intArrayOf(1, 1, 0),
-//        intArrayOf(1, 0, 1),
-//    )
+    val points = arrayOf(
+        intArrayOf(3, 2),
+        intArrayOf(6, 4),
+        intArrayOf(4, 7),
+        intArrayOf(1, 4),
+    )
+    val routes = arrayOf(
+        intArrayOf(4, 2),
+        intArrayOf(1, 3),
+        intArrayOf(4, 2),
+        intArrayOf(4, 3),
+    )
     val key = arrayOf(
-        intArrayOf(1,0),
-        intArrayOf(0,0)
+        intArrayOf(1, 0),
+        intArrayOf(0, 0)
     )
 
     val lock = arrayOf(
-        intArrayOf(0,0),
-        intArrayOf(1,1)
+        intArrayOf(0, 0),
+        intArrayOf(1, 1)
     )
 
-    val answer = LockAndKey().solution(key, lock)
+    val answer = FindCollisionRiskSecond().solution(points, routes)
     println(answer)
 
 //    // 백준
